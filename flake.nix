9bca60748781190f58bab9063d28aec050d92d0c
Krass, Leon <leon.krass@sva.de>
2025-08-20 15:47:57 +0200

    include helmfile

--- flake.nix
+++ flake.nix
@@ -8,17 +8,37 @@
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-      });
+      });      
     in
     {
-      devShells = forEachSupportedSystem ({ pkgs }: {
+      devShells = forEachSupportedSystem ({ pkgs }: 
+        let
+        # wrap helm to include plugins for helmfile
+          wrapped-kubernetes-helm = with pkgs; wrapHelm kubernetes-helm {
+            plugins = with kubernetes-helmPlugins; [
+              helm-secrets
+              helm-diff
+              helm-s3
+              helm-git
+            ];  
+          };  
+
+          # pass plugins to helmfile-wrapped package
+          wrapped-helmfile = pkgs.helmfile-wrapped.override {
+            inherit (wrapped-kubernetes-helm) pluginsDir;
+          };  
+
+      in
+      {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            kubernetes-helm
             kubectl
           ];
-          shellHook = ''
-          '';
+
+          buildInputs = [
+              wrapped-kubernetes-helm
+              wrapped-helmfile
+          ];
         };
       });
     };
